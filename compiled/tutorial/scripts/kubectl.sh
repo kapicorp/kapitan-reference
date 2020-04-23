af9136eacff0e626827afc745c922b5289353b4a
Alessandro De Maria <a.demaria@synthace.com>
2020-04-23 14:52:27 +0100

    added initial setup for kind

---
+++ compiled/tutorial/scripts/kubectl.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+DIR=$(dirname ${BASH_SOURCE[0]})
+${DIR}/setup_context.sh >/dev/null
+if [[ -p /dev/stdin ]]
+then
+    INPUT=$( cat )
+fi
+KUBECTL="kubectl --context tutorial-kind"
+echo "${INPUT}" | ${KUBECTL} "$@"
---
+++ compiled/tutorial/scripts/setup_context.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -o nounset -o pipefail -o noclobber -o errexit
+KUBECTL="kubectl"
+
+
