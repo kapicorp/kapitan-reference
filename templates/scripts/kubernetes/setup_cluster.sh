35e01881424585affe63a5f1fa8c721ba08d11b1
ademariag <a.demaria@synthace.com>
2020-04-29 00:15:50 +0100

    Add kind cluster support (#13)

--- templates/scripts/kubernetes/setup_cluster.sh
+++ templates/scripts/kubernetes/setup_cluster.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -o nounset -o pipefail -o noclobber -o errexit
-GCLOUD="gcloud"
 
 {% set p = inventory.parameters %}
 
 {% set cluster = p.cluster %}
 {% if cluster.type == "gke" %}
+GCLOUD="gcloud"
 ${GCLOUD} container clusters get-credentials {{cluster.name}} --zone {{cluster.zone}} --project {{cluster.google_project}}
+{% elif cluster.type == "kind" %}
+KIND="kind"
+$KIND create cluster -q --name {{cluster.name}} || echo "Kind cluster {{cluster.name}} already exists!"
+$KIND export kubeconfig
 {% endif %}
