238add24e179e1b7a8adc10794be887245f8ad50
Alessandro De Maria <ademaria@kapicorp.com>
2021-03-08 22:23:13 +0000

    change generator path

--- components/generators/terraform/__init__.py
+++ components/generators/terraform/__init__.py
@@ -5,6 +5,12 @@ inv = inventory()
 
 def main(input_params):
     obj = BaseObj()
-    for resource_name, content in inv.parameters.kapicorp.items():
+    generator_root_paths = input_params.get("generator_root", "resources.tf").split(".")
+    root = inv.parameters
+
+    for path in generator_root_paths:
+        root = root.get(path, {})
+
+    for resource_name, content in root.items():
         obj.root["{}.tf".format(resource_name)][resource_name] = content
     return obj
