3672853e1b319df0cb546d7b1723fdce7ad59982
Alessandro De Maria <ademaria@kapicorp.com>
2021-03-19 22:05:31 +0000

    Fix filenames

--- components/generators/terraform/__init__.py
+++ components/generators/terraform/__init__.py
@@ -5,12 +5,16 @@ inv = inventory()
 
 def main(input_params):
     obj = BaseObj()
-    generator_root_paths = input_params.get("generator_root", "resources.tf").split(".")
+    generator_root_paths = input_params.get("generator_root", "sections.tf").split(".")
     root = inv.parameters
 
     for path in generator_root_paths:
         root = root.get(path, {})
 
-    for resource_name, content in root.items():
-        obj.root["{}.tf".format(resource_name)][resource_name] = content
+    for section_name, content in root.items():
+        if section_name in [ "resource", "data" ]:
+            for resource_name, content in content.items():
+               obj.root["{}.tf".format(resource_name)][section_name][resource_name] = content 
+        else:
+            obj.root["{}.tf".format(section_name)][section_name] = content
     return obj
